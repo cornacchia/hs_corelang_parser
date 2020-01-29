e3ca4482e205ff57f4b8028976adc2ce4ad5ce0e
cornacchia <pozzan.gabriele@gmail.com>
2020-01-29 16:26:48 +0100

    Remove some unused definitions from parseProg

--- ParseProg.hs
+++ ParseProg.hs
@@ -21,34 +21,13 @@ data Expr a
 
 type Def a = (a, Expr a)
 
--- tag, list bound variables, rhs expression
 type Alter a = (Int, [a], Expr a)
 data IsRec = NonRecursive | Recursive
   deriving Show
 
--- this function is used to identify expressions with no internal structure
-isAtomicExpr :: Expr a -> Bool
-isAtomicExpr (EVar _) = True
-isAtomicExpr (ENum _) = True
-isAtomicExpr _ = False
-
-type CoreExpr = Expr Name
-
--- extract variable names from a list of definitions
-bindersOf :: [(a, b)] -> [a]
-bindersOf definitions = [name | (name, rhs) <- definitions]
-
--- extract right hand sides from a list of definitions
-rhhsOf :: [(a, b)] -> [b]
-rhhsOf definitions = [rhs | (name, rhs) <- definitions]
-
--- a Core program is a list of supercombinator definitions
 type Program a = [ScDefn a]
-type CoreProgram = Program Name
 
--- name of the supercombinator, arguments, body
 type ScDefn a = (Name, [a], Expr a)
-type CoreScDefn = ScDefn Name
 
 reserved :: String -> Bool
 reserved s = elem s reservedWords
